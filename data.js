662674391a3e83f97d67c36f0ec83aa1ad44d7b5
alaaelmisry <3laaelmisry@gmail.com>
2026-08-05 01:29:59 +0300

    Initialize vehicle data for fleet management
    Added vehicle data for fleet management including private vehicles, trucks, special vehicles, and generators.

---
+++ data.js
@@ -0,0 +1,683 @@
+/************************************************
+                ATP FLEET MANAGEMENT
+************************************************/
+
+const vehicles = [
+
+/*----------------------------------------
+المركبات الصغيرة
+----------------------------------------*/
+
+{
+    id:1,
+    type:"PRIVATE",
+    number:"7983",
+    driver:"ALAA",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:2,
+    type:"PRIVATE",
+    number:"2175",
+    driver:"AYMAN",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:3,
+    type:"PRIVATE",
+    number:"2181",
+    driver:"TARIQ",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:4,
+    type:"PRIVATE",
+    number:"9306",
+    driver:"AMMAR",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:5,
+    type:"PRIVATE",
+    number:"9311",
+    driver:"ARGY",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:6,
+    type:"PRIVATE",
+    number:"9520",
+    driver:"MOSTFA",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:7,
+    type:"PRIVATE",
+    number:"9521",
+    driver:"SHBAZ",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:8,
+    type:"PRIVATE",
+    number:"6163",
+    driver:"OKAIRY",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:9,
+    type:"PRIVATE",
+    number:"6167",
+    driver:"KHODAIR",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:10,
+    type:"PRIVATE",
+    number:"2190",
+    driver:"WAGHAT",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+  {
+    id:11,
+    type:"PRIVATE",
+    number:"2204",
+    driver:"SAJJAD",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:12,
+    type:"PRIVATE",
+    number:"2141",
+    driver:"ALADIN",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:13,
+    type:"PRIVATE",
+    number:"2199",
+    driver:"REDA",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:14,
+    type:"PRIVATE",
+    number:"4682",
+    driver:"JACOUB",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:15,
+    type:"PRIVATE",
+    number:"4683",
+    driver:"ZESHAN",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:16,
+    type:"PRIVATE",
+    number:"4688",
+    driver:"ZESHAN",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:17,
+    type:"PRIVATE",
+    number:"4685",
+    driver:"WAQAS",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:18,
+    type:"PRIVATE",
+    number:"5383",
+    driver:"MUDATHR",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:19,
+    type:"PRIVATE",
+    number:"6102",
+    driver:"ADLY",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:20,
+    type:"PRIVATE",
+    number:"7843",
+    driver:"FARES",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:21,
+    type:"PRIVATE",
+    number:"8191",
+    driver:"ARFA",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:22,
+    type:"PRIVATE",
+    number:"4284",
+    driver:"GAZALY",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+/*----------------------------------------
+رؤوس الشاحنات والقاطرات
+----------------------------------------*/
+
+{
+    id:23,
+    type:"TRUCKS",
+    number:"4552",
+    driver:"RAFIQ",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:24,
+    type:"TRUCKS",
+    number:"4553",
+    driver:"SAHBAZ",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:25,
+    type:"TRUCKS",
+    number:"4554",
+    driver:"ADHAM",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:26,
+    type:"TRUCKS",
+    number:"4556",
+    driver:"BAKR",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:27,
+    type:"TRUCKS",
+    number:"4557",
+    driver:"ZIA UR",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:28,
+    type:"TRUCKS",
+    number:"4558",
+    driver:"RADI",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:29,
+    type:"TRUCKS",
+    number:"4559",
+    driver:"RAMDAN",
+    driverStatus:"present",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:30,
+    type:"TRUCKS",
+    number:"2924",
+    driver:"NO DRIVER",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+  {
+    id:1,
+    type:"PRIVATE",
+    number:"7983",
+    driver:"ALAA",
+
+    driverStatus:"present",
+    maintenance:"none",
+
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+  /*----------------------------------------
+السيارات المتخصصة
+----------------------------------------*/
+
+{
+    id:68,
+    type:"SPECIAL",
+    number:"SP01",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:69,
+    type:"SPECIAL",
+    number:"SP02",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:70,
+    type:"SPECIAL",
+    number:"SP03",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:71,
+    type:"SPECIAL",
+    number:"SP04",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:72,
+    type:"SPECIAL",
+    number:"SP05",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:73,
+    type:"SPECIAL",
+    number:"SP06",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:74,
+    type:"SPECIAL",
+    number:"SP07",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:75,
+    type:"SPECIAL",
+    number:"SP08",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:76,
+    type:"SPECIAL",
+    number:"SP09",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:77,
+    type:"SPECIAL",
+    number:"SP10",
+    driver:"NO DRIVER",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+  /*----------------------------------------
+المولدات والمضخات
+----------------------------------------*/
+
+{
+    id:78,
+    type:"GENERATORS",
+    number:"GEN01",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:79,
+    type:"GENERATORS",
+    number:"GEN02",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:80,
+    type:"GENERATORS",
+    number:"GEN03",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:81,
+    type:"GENERATORS",
+    number:"GEN04",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:82,
+    type:"GENERATORS",
+    number:"GEN05",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:83,
+    type:"GENERATORS",
+    number:"GEN06",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:84,
+    type:"GENERATORS",
+    number:"GEN07",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:85,
+    type:"GENERATORS",
+    number:"GEN08",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:86,
+    type:"GENERATORS",
+    number:"GEN09",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id:87,
+    type:"GENERATORS",
+    number:"GEN10",
+    driver:"NO OPERATOR",
+    driverStatus:"present",
+    maintenance:"none",
+    status:"working",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+}
+  
