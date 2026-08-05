346504cc53b21b0f0751aee5624287dbc164e88a
alaaelmisry <3laaelmisry@gmail.com>
2026-08-05 15:22:45 +0300

    Update data.js

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ PICKUP-SEDAN
 ----------------------------------------*/
 
 {
-    id:1,
+    id:11,
     type:"PRIVATE",
     number:"7983",
     driver:"ALAA",
@@ -22,7 +22,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:2,
+    id:12,
     type:"PRIVATE",
     number:"2175",
     driver:"AYMAN",
@@ -35,7 +35,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:3,
+    id:13,
     type:"PRIVATE",
     number:"2181",
     driver:"TARIQ",
@@ -48,7 +48,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:4,
+    id:14,
     type:"PRIVATE",
     number:"9306",
     driver:"AMMAR",
@@ -61,7 +61,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:5,
+    id:15,
     type:"PRIVATE",
     number:"9311",
     driver:"ARGY",
@@ -74,7 +74,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:6,
+    id:16,
     type:"PRIVATE",
     number:"9520",
     driver:"MOSTFA",
@@ -87,7 +87,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:7,
+    id:17,
     type:"PRIVATE",
     number:"9521",
     driver:"SHBAZ",
@@ -100,7 +100,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:8,
+    id:18,
     type:"PRIVATE",
     number:"6163",
     driver:"OKAIRY",
@@ -113,7 +113,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:9,
+    id:19,
     type:"PRIVATE",
     number:"6167",
     driver:"KHODAIR",
@@ -126,7 +126,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:10,
+    id:110,
     type:"PRIVATE",
     number:"2190",
     driver:"WAGHAT",
@@ -138,7 +138,7 @@ PICKUP-SEDAN
     lastUpdate:""
 },
   {
-    id:11,
+    id:111,
     type:"PRIVATE",
     number:"2204",
     driver:"SAJJAD",
@@ -151,7 +151,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:12,
+    id:112,
     type:"PRIVATE",
     number:"2141",
     driver:"ALADIN",
@@ -164,7 +164,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:13,
+    id:113,
     type:"PRIVATE",
     number:"2199",
     driver:"REDA",
@@ -177,7 +177,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:14,
+    id:114,
     type:"PRIVATE",
     number:"4682",
     driver:"JACOUB",
@@ -190,7 +190,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:15,
+    id:115,
     type:"PRIVATE",
     number:"4683",
     driver:"ZESHAN",
@@ -203,7 +203,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:16,
+    id:116,
     type:"PRIVATE",
     number:"4688",
     driver:"ZESHAN",
@@ -216,7 +216,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:17,
+    id:117,
     type:"PRIVATE",
     number:"4685",
     driver:"WAQAS",
@@ -229,7 +229,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:18,
+    id:118,
     type:"PRIVATE",
     number:"5383",
     driver:"MUDATHR",
@@ -242,7 +242,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:19,
+    id:119,
     type:"PRIVATE",
     number:"6102",
     driver:"ADLY",
@@ -255,7 +255,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:20,
+    id:120,
     type:"PRIVATE",
     number:"7843",
     driver:"FARES",
@@ -268,7 +268,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:21,
+    id:121,
     type:"PRIVATE",
     number:"8191",
     driver:"ARFA",
@@ -281,7 +281,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:22,
+    id:122,
     type:"PRIVATE",
     number:"4284",
     driver:"GAZALY",
@@ -298,7 +298,7 @@ PICKUP-SEDAN
 ----------------------------------------*/
 
 {
-    id:23,
+    id:21,
     type:"TRUCKS",
     number:"4552",
     driver:"RAFIQ",
@@ -311,7 +311,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:24,
+    id:22,
     type:"TRUCKS",
     number:"4553",
     driver:"SAHBAZ",
@@ -324,7 +324,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:25,
+    id:23,
     type:"TRUCKS",
     number:"4554",
     driver:"THAQB",
@@ -337,7 +337,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:26,
+    id:24,
     type:"TRUCKS",
     number:"4556",
     driver:"BAKR",
@@ -350,7 +350,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:27,
+    id:25,
     type:"TRUCKS",
     number:"4557",
     driver:"ZIA UR",
@@ -363,7 +363,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:28,
+    id:26,
     type:"TRUCKS",
     number:"4558",
     driver:"ASFOR",
@@ -376,7 +376,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:29,
+    id:27,
     type:"TRUCKS",
     number:"4559",
     driver:"RAMDAN",
@@ -389,7 +389,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:30,
+    id:28,
     type:"TRUCKS",
     number:"2924",
     driver:"NO DRIVER",
@@ -402,7 +402,7 @@ PICKUP-SEDAN
 },
 
   {
-    id:31,
+    id:29,
     type:"TRUCKS",
     number:"2932",
     driver:"MUDATHR",
@@ -415,7 +415,7 @@ PICKUP-SEDAN
 },
 
   {
-    id:32,
+    id:210,
     type:"TRUCKS",
     number:"2950",
     driver:"NO DRIVER",
@@ -428,7 +428,7 @@ PICKUP-SEDAN
 },
 
   {
-    id:33,
+    id:211,
     type:"TRUCKS",
     number:"4676",
     driver:"NO DRIVER",
@@ -441,7 +441,7 @@ PICKUP-SEDAN
 },
 
   {
-    id:34,
+    id:212,
     type:"TRUCKS",
     number:"4677",
     driver:"SAMIULLAH",
@@ -454,7 +454,7 @@ PICKUP-SEDAN
 },
 
   {
-    id:35,
+    id:213,
     type:"TRUCKS",
     number:"2062",
     driver:"SHEVNDR",
@@ -467,7 +467,7 @@ PICKUP-SEDAN
 },
 
   {
-    id:36,
+    id:214,
     type:"TRUCKS",
     number:"2087",
     driver:"KISHOR",
@@ -480,7 +480,7 @@ PICKUP-SEDAN
 },
 
   {
-    id:37,
+    id:215,
     type:"TRUCKS",
     number:"6031",
     driver:"MENAKHAN",
@@ -493,7 +493,7 @@ PICKUP-SEDAN
 },
 
   {
-    id:38,
+    id:216,
     type:"TRUCKS",
     number:"6032",
     driver:"LAKBA",
@@ -506,7 +506,7 @@ PICKUP-SEDAN
 },
 
    {
-    id:39,
+    id:217,
     type:"TRUCKS",
     number:"6033",
     driver:"SAFIL",
@@ -519,7 +519,7 @@ PICKUP-SEDAN
 },
 
    {
-    id:40,
+    id:218,
     type:"TRUCKS",
     number:"3162",
     driver:"NO DRIVER",
@@ -531,6 +531,246 @@ PICKUP-SEDAN
     lastUpdate:""
 },
   
+/*----------------------------------------
+المعدات الكبيرة والصغيرة
+----------------------------------------*/
+
+{
+    id: 31,
+    type: "EQUIPMENT",
+    number: "1817",
+    driver: "PADAM",
+   driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 32,
+    type: "EQUIPMENT",
+    number: "2400",
+    driver: "ADHAM",
+   driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 33,
+    type: "EQUIPMENT",
+    number: "6219",
+    driver: "RAM KUMAR",
+   driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+  {
+    id: 34,
+    type: "EQUIPMENT",
+    number: "6900",
+    driver: "PAPUSH",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 35,
+    type: "EQUIPMENT",
+    number: "7470",
+    driver: "OTHMAN",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 36,
+    type: "EQUIPMENT",
+    number: "9956",
+    driver: "ZOHIB",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 37,
+    type: "EQUIPMENT",
+    number: "7772",
+    driver: "NO OPERATOR",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 38,
+    type: "EQUIPMENT",
+    number: "1384",
+    driver: "SUNIL",
+   driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 39,
+    type: "EQUIPMENT",
+    number: "HITACHI",
+    driver: "MANSOR",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 310,
+    type: "EQUIPMENT",
+    number: "7674",
+    driver: "KHUSIRAM",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 311,
+    type: "EQUIPMENT",
+    number: "GCB1",
+    driver: "GULAM F",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 312,
+    type: "EQUIPMENT",
+    number: "GCB2",
+    driver: "NO OPERATOR",
+   driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 313,
+    type: "EQUIPMENT",
+    number: "1182",
+    driver: "RAJA",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 314,
+    type: "EQUIPMENT",
+    number: "6023",
+    driver: "NO OPERATOR",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 315,
+    type: "EQUIPMENT",
+    number: "FORKLIFT",
+    driver: "NOUR",
+   driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 316,
+    type: "EQUIPMENT",
+    number: "BOBCAT",
+    driver: "OTHMAN",
+    driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 317,
+    type: "EQUIPMENT",
+    number: "BOMAG1",
+    driver: "LALBABU",
+   driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+},
+
+{
+    id: 318,
+    type: "EQUIPMENT",
+    number: "BOMAG2",
+    driver: "LUQMAN",
+   driverStatus:"absent",
+    status:"working",
+    maintenance:"none",
+    reason:"",
+    notes:"",
+    lastUpdate:""
+}
+
+];
+
   
   /*----------------------------------------
 السيارات المتخصصة
@@ -646,7 +886,7 @@ PICKUP-SEDAN
 ----------------------------------------*/
 
 {
-    id:49,
+    id:51,
     type:"GENERATORS",
     number:"124KVA",
     driver:"WAJHAT",
@@ -659,7 +899,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:50,
+    id:52,
     type:"GENERATORS",
     number:"91KVA",
     driver:"WAJHAT",
@@ -672,7 +912,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:51,
+    id:53,
     type:"GENERATORS",
     number:"34KVA",
     driver:"WAJHAT",
@@ -685,7 +925,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:52,
+    id:54,
     type:"GENERATORS",
     number:"24KVA",
     driver:"WAJHAT",
@@ -698,7 +938,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:53,
+    id:55,
     type:"GENERATORS",
     number:"TOWER LIGHT1",
     driver:"WAJHAT",
@@ -711,7 +951,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:54,
+    id:56,
     type:"GENERATORS",
     number:"TOWER LIGHT2",
     driver:"NO OPERATOR",
@@ -724,7 +964,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:55,
+    id:57,
     type:"GENERATORS",
     number:"TOWER LIGHT3",
     driver:"NO OPERATOR",
@@ -737,7 +977,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:56,
+    id:58,
     type:"GENERATORS",
     number:"TOWER LIGHT4",
     driver:"NO OPERATOR",
@@ -750,7 +990,7 @@ PICKUP-SEDAN
 },
 
 {
-    id:57,
+    id:59,
     type:"GENERATORS",
     number:"TOWER LIGHT5",
     driver:"NO OPERATOR",
